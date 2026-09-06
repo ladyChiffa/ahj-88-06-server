4833e1fff2f020d9c94b31e64bcc4954fdeddc48
Oksana (Kes) Milenina <OkMilenina@yandex.ru>
2026-09-06 22:40:52 +0300

    Усложнение сервера - смотрим POST запрос

--- server.js
+++ server.js
@@ -3,7 +3,19 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     console.log(req.url);
     console.log(req.headers);
-    res.end();
+    
+    const buffer = [];
+    
+    req.on('data', (chunk) => {
+        buffer.push(chunk);
+    });
+    
+    req.on('end', () => {
+        const data = Buffer.concat(buffer).toString();
+        console.log(data);
+    });
+
+    res.end('server response')
 });
 
 const port = 8080;
