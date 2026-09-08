befc019e10349c793ba46c9627884c17bf0a8471
Oksana (Kes) Milenina <OkMilenina@yandex.ru>
2026-09-08 13:59:56 +0300

    Добавляем отписку (+коррекция ошибок, +удаление лишнего app.use, +рефакторинг)

--- server.js
+++ server.js
@@ -4,35 +4,80 @@ const {koaBody} = require('koa-body');
 
 const app = new Koa();
 
-const subscriptions = [];
+let subscriptions = [];
 
 app.use(koaBody({
-        urlencoded: true
+        urlencoded: true,
+        parsedMethods: ['POST', 'PUT', 'PATCH', 'GET', 'HEAD', 'DELETE']
 }));
 
+
 app.use((ctx, next) => {
+    console.log('OPTIONS CHECK METHOD PROCESSING')
+    if (ctx.request.method !== 'OPTIONS') {
+        console.log('----- skipped')
+        next();
+        return;
+    }
+
+    console.log('----- done')
+    ctx.response.set('Access-Control-Allow-Origin', '*');
+    ctx.response.set('Access-Control-Allow-Methods', 'DELETE, PUT, PATCH, GET, POST');
+    ctx.response.status = 204;
+});
+
+
+app.use((ctx, next) => {
+    console.log('REQUEST DATA METHOD')
     console.log(ctx.headers)
     console.log(ctx.request.query)
     console.log(ctx.request.body)
+    ctx.response.set('Access-Control-Allow-Origin', '*');
+    next();
+});
 
-    const {name, body} = ctx.request.body;
+app.use((ctx, next) => {
+    console.log('POST METHOD')
+    if (ctx.request.method !== 'POST') {
+        console.log('----- skipped')
+        next();
+        return;
+    }
+    console.log('----- done')
+
+    const {name, phone} = ctx.request.body;
     if (subscriptions.some(sub => sub.phone === phone)) {
         ctx.response.status = 400;
         ctx.response.body = 'subscription exists';
     }
     else {
         subscriptions.push({name, phone});
-        ctx.response.body = 'server response';
+        ctx.response.body = 'OK';
     }
 
-    ctx.response.set('Access-Control-Allow-Origin', '*');
     next();
 });
 
-app.use((ctx) => { 
-    console.log(ctx.headers)
+app.use((ctx, next) => {
+    console.log('DELETE METHOD')
+    if (ctx.request.method !== 'DELETE') {
+        console.log('----- skipped')
+        next();
+        return;
+    }
+    console.log('----- done')
 
-    ctx.response.body = 'i am the second middleware'
+    const {name, phone} = ctx.request.body;
+    if (subscriptions.every(sub => sub.phone !== phone)) {
+        ctx.response.status = 400;
+        ctx.response.body = 'subscription doesnt exist';
+    }
+    else {
+        subscriptions = subscriptions.filter(sub => sub.phone !== phone);
+        ctx.response.body = 'OK';
+    }
+
+    next();
 });
 
 
