f51fff89fd275ba130c544fd13785bb147bc74f6
Oksana (Kes) Milenina <OkMilenina@yandex.ru>
2026-09-09 23:25:16 +0300

    Передача файлов на сервер с красивым ответом

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const http = require('http');
 const Koa = require('koa');
 const {koaBody} = require('koa-body');
+const koaStatic = require('koa-static');
 const path = require('path');
 const fs = require('fs');
-
+const uuid = require('uuid');
 
 const app = new Koa();
 
 let subscriptions = [];
+const public = path.join(__dirname, 'public');
+
+app.use(koaStatic(public));
 
 app.use(koaBody({
         urlencoded: true,
@@ -52,17 +56,21 @@ app.use((ctx, next) => {
     console.log('----- done')
 
     console.log(ctx.request.files);
+    let fileName;
 
     try {
-        const public = path.join(__dirname, '/public');
         const {file} = ctx.request.files;
-        fs.copyFileSync(file.filepath, public + '/' + file.originalFilename);
+        const subfolder = uuid.v4();
+        const uploadFolder = public + '/' + subfolder
+        fs.mkdirSync(uploadFolder);
+        fs.copyFileSync(file.filepath, uploadFolder + '/' + file.originalFilename);
+        fileName = '/' + subfolder + '/' + file.originalFilename;
     }
     catch (error) {
         ctx.response.status = 500;
         return;
     }
-    ctx.response.body = 'OK';
+    ctx.response.body = fileName;
     next();
 });
 
